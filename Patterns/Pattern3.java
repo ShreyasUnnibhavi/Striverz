293041465249eb542f80baaa0ef9cd9e077fa9aa
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-07-28 16:55:44 +0530

    Right angled triangle with numbers based on inner loop

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,14 @@
+public class Pattern3 {
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + 1);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    }
+}
